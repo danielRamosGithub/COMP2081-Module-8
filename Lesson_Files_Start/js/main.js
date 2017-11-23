067c0e6c88c58e344b6b0ca301c30a6f7beb4dd7
Daniel Ramos <danielramos@Daniels-MacBook-Pro.local>
2017-11-23 14:23:01 -0500

    Files for module 8 and Lab_8.

---
+++ Lesson_Files_Start/js/main.js
@@ -0,0 +1,39 @@
+// STEP 3: Wait until the DOM is ready
+
+	// STEP 4: Get canvas and set context to 2d
+
+	// STEP 5: Draw line
+
+	// STEP 6: Draw rectangle
+
+	// STEP 7: Fill and outline rectangle
+
+	// STEP 8: Draw circle
+
+	// arc(x, y, r, [length in radians], counterclockwise?)
+	// https://www.mathsisfun.com/geometry/radians.html
+
+	// STEP 9: Fill and outline circle
+
+	//  STEP 10: Draw arc
+
+	// To go from degrees to radians: multiply by π, divide by 180
+
+	// STEP 11: Draw a gradient
+
+	// STEP 12: Draw a triangle
+
+	// starting point
+
+	// move down and right
+
+	// move up
+
+	// fill the shape in
+
+	// Head back to index.html for the next STEP
+
+	// STEP 15: Build out the data object
+	/* Many thanks to Dan Wahlin's great tutorial at https://weblogs.asp.net/dwahlin/creating-a-line-chart-using-the-html-5-canvas */
+
+	/* STEP 16: Render the chart on the canvas element - all done! */
